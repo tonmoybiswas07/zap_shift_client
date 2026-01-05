674fc77b58f193f28a11718e6f1c29e60fb44762
Tonmoy Biswas <tonmoybiswas@Tonmoys-MacBook-Air.local>
2026-01-05 19:01:34 +0600

    update coverage page

--- src/Pages/Coverage/Coverage.jsx
+++ src/Pages/Coverage/Coverage.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Coverage = () => {
     return (
-        <div>
-            this is coverage page
+        <div className='bg-white p-10 my-10 rounded-2xl'>
+            <div>
+                <h2 className='text-5xl font-extrabold'>We are available in 64 districts</h2>
+            </div>
+            <div className='flex justify-start items-start mt-5'>
+                <div>
+                <input className='bg-gray-200 border-2 border-[#CAEB66] rounded-2xl h-10 w-70 pl-3' placeholder='search here' type="search" name="" id="" />
+            </div>
+            <button className='btn rounded-2xl -ml-6 bg-[#CAEB66] border-2 border-[#CAEB66]'>Search</button>
+            </div>
+            <hr  className='mt-10 text-gray-300'/>
         </div>
     );
 };
